c401e08444d02f193ff14e887807252ca3f11bd3
Megha Rajeev <106422400+meghaarajeev@users.noreply.github.com>
2024-03-06 00:40:22 +0530

    Create 6.Building a Simple Word Replacement Pgm.py

---
+++ Codes/6.Building a Simple Word Replacement Pgm.py
@@ -0,0 +1,5 @@
+sentence = input("Enter your Sentence:")
+print("Your Sentence is :"+sentence)
+word1= input("Enter word to replace:")
+word2= input("Enter word to replace it with:")
+print("sentence.replace(word1,word2))
