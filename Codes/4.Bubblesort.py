c600fcc84cfe72bf0ed431f84ed2aad3758122ce
Megha Rajeev <106422400+meghaarajeev@users.noreply.github.com>
2024-05-31 20:17:57 +0530

    Update 4.Bubblesort.py

--- Codes/4.Bubblesort.py
+++ Codes/4.Bubblesort.py
@@ -1,11 +1,11 @@
-a=[]
-n = int(input("Enter limit:"))
+a=[] #creating a list
+n = int(input("Enter limit:")) 
 for i in range(0,n):
     x=int(input("Enter Numbers:"))
-    a.append(x)
+    a.append(x)  #inserting values to the list
 for i in range(0,n):
     for j in  range(0,n-i-1):
         if a[j] > a[j+1]:
-            a[j],a[j+1] = a[j+1],a[j]
+            a[j],a[j+1] = a[j+1],a[j] #sorting statement
 
 print(a)
