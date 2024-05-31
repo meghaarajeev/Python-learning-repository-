89207f49ce355dd7ce08bd695e119487b6cfed77
Megha Rajeev <106422400+meghaarajeev@users.noreply.github.com>
2024-05-31 20:36:04 +0530

    Update 9. Pattern.py

--- Codes/9. Pattern.py
+++ Codes/9. Pattern.py
@@ -1,6 +1,15 @@
 n = int(input("Enter limit:"))
-for i in range(1,n+1):
+for i in range(0,n+1):
     for j in range(0,i+1):
         print("*",end=" ")
     print()
-    
+
+
+Output :
+Enter Limit : 6
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * *
