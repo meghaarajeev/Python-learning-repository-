2c2ac5907ae5332c149e3abf8a643dc890364fd3
Megha Rajeev <106422400+meghaarajeev@users.noreply.github.com>
2024-06-24 16:53:48 +0530

    Update 10.Pattern2.py

--- Codes/10.Pattern2.py
+++ Codes/10.Pattern2.py
@@ -1,3 +1,4 @@
+1)
 n = int(input("limit:"))
 for i in range(n,0,-1):
     for j in range(1,i+1):
@@ -14,3 +15,27 @@ limit: 6
 * * * 
 * * 
 * 
+
+2)
+for i in range(4,0,-1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+output:
+1 2 3 4 
+1 2 3 
+1 2 
+1 
+
+3)
+for i in range(4,0,-1):
+    for j in range(1,i+1):
+        print(i,end=" ")
+    print()
+
+output:
+4 4 4 4 
+3 3 3 
+2 2 
+1
